b51433a833085f4835202d5ef96b7f7937877885
Rocky111245 <rakibalhasan170@gmail.com>
2024-01-06 06:36:42 +0800

    Merge Functions Added

--- Hybrid Timsort Algorithm.c
+++ Hybrid Timsort Algorithm.c
@@ -5,40 +5,51 @@
 #include <string.h>
 #include <stdbool.h>
 
-
 struct RunIndices {
     int indices;
     int length;
 };
 typedef struct RunIndices runIndices;
 
-//
 int runMaker(int array[], runIndices runs[], int sizeBig, int minSize, int maxSize);
-int* binaryInsert(int firstArray[], int firstSize, int element);
+int *binaryInsert(int firstArray[], int firstSize, int element);
 void ascending(int *i, int sizeBig, int minSize, int array[]);
-void descendingFunction(int *i, int sizeBig, int minSize, int array[],int *start_descending, bool *descending);
+void descendingFunction(int *i, int sizeBig, int minSize, int array[], int *start_descending, bool *descending);
+int binarySearch(int array[], int low, int high, int target);
+int gallop(int start, int end, int array[], int target);
 
-int main(){
+int main() {
     clock_t start, end; // Declare variables to hold the start and end times
     double cpu_time_used;
     start = clock(); // Get the start time
-    int array[] = {29, 1065, 848, 818, 805, 795, 747, 734, 712, 710, 674, 664, 646, 627, 609, 586, 532, 506, 392, 343, 316, 285, 223, 149, 107, 73, 28, 9, 4411, 2356, 7150, 1916, 1703, 2901, 2350, 9304, 6563, 2547, 3622, 155, 1831, 3562, 6873, 8507, 7658, 7453, 5518, 4855, 3005, 1322, 7654, 4570, 744, 6119, 3685, 6598, 9117, 6477, 1593, 7621, 6522, 7884, 3025, 6185, 1163, 7858, 4549, 8451, 5863, 5432, 2561, 1042, 3202, 1450, 7431, 698, 4031, 1974, 4641, 3970, 5093, 5213, 9666, 2512, 3323, 8463, 1049, 8227, 8638, 7973, 5552, 1057, 7811, 2076, 4600, 3190, 3574, 71, 3799, 1438, 5799, 5699, 8898, 7863, 7485, 1576, 6399, 2851, 3847, 1023, 5013, 9102, 7735, 554, 156, 2672, 2599, 7283, 6353, 1113, 9855, 4028, 9553, 6730, 5, 9170, 964, 6550, 2108, 9856, 9074, 4889, 5597, 3747, 8196, 8376, 822, 8583, 7373, 2554, 6006, 7519, 1949, 6762, 6801, 2910, 4562, 5654, 6519, 6377, 6665, 6138, 7139, 2732, 4691, 9200, 6000, 6535, 5131, 4416, 5337, 6183, 2393, 3660, 8665, 463, 9150, 8223, 441, 1308, 471, 3645, 9782, 4035, 5527, 9136, 2354, 6266, 4846, 8589, 2264, 5697, 5782, 5951, 3114, 9480, 8370, 9385, 1018, 5442, 2150, 951, 3562, 884, 9, 6762, 6266, 7852, 439};
-    int sizeBig=sizeof(array)/sizeof(array[0]);
-    int k=sizeBig;
+    int array[] = {29, 1065, 848, 818, 805, 795, 747, 734, 712, 710, 674, 664, 646, 627, 609, 586, 532, 506, 392, 343, 316, 285, 223, 149, 107, 73, 28, 9, 4411, 2356, 7150, 1916, 1703, 2901, 2350,
+                   9304, 6563, 2547, 3622, 155, 1831, 3562, 6873, 8507, 7658, 7453, 5518, 4855, 3005, 1322, 7654, 4570, 744, 6119, 3685, 6598, 9117, 6477, 1593, 7621, 6522, 7884, 3025, 6185, 1163,
+                   7858, 4549, 8451, 5863, 5432, 2561, 1042, 3202, 1450, 7431, 698, 4031, 1974, 4641, 3970, 5093, 5213, 9666, 2512, 3323, 8463, 1049, 8227, 8638, 7973, 5552, 1057, 7811, 2076, 4600,
+                   3190, 3574, 71, 3799, 1438, 5799, 5699, 8898, 7863, 7485, 1576, 6399, 2851, 3847, 1023, 5013, 9102, 7735, 554, 156, 2672, 2599, 7283, 6353, 1113, 9855, 4028, 9553, 6730, 5, 9170,
+                   964, 6550, 2108, 9856, 9074, 4889, 5597, 3747, 8196, 8376, 822, 8583, 7373, 2554, 6006, 7519, 1949, 6762, 6801, 2910, 4562, 5654, 6519, 6377, 6665, 6138, 7139, 2732, 4691, 9200,
+                   6000, 6535, 5131, 4416, 5337, 6183, 2393, 3660, 8665, 463, 9150, 8223, 441, 1308, 471, 3645, 9782, 4035, 5527, 9136, 2354, 6266, 4846, 8589, 2264, 5697, 5782, 5951, 3114, 9480,
+                   8370, 9385, 1018, 5442, 2150, 951, 3562, 884, 9, 6762, 6266, 7852, 439};
+    int sizeBig = sizeof(array) / sizeof(array[0]);
+    int k = sizeBig;
     runIndices runs[70];
+    runIndices mergedRuns[70];
     int y;
-    for(y = 0; y < 70; y++) {
+    for (y = 0; y < 70; y++) {
         runs[y].indices = 0;
         runs[y].length = 0;
     }
-    int count=0;
+    for (y = 0; y < 70; y++) {
+        mergedRuns[y].indices = 0;
+        mergedRuns[y].length = 0;
+    }
+    int count = 0;
     // Calculate the nearest power of 2 for the number of runs
-    while(k != 1) {
-        k = k/2;
+    while (k != 1) {
+        k = k / 2;
         count++;
     }
     int numberOfRuns = pow(2, count - 1); // Adjusting count to get the closest lower power of 2
-    float avgRunSize = (float)sizeBig / numberOfRuns;
+    float avgRunSize = (float) sizeBig / numberOfRuns;
     // Establishing minimum and maximum sizes for the runs
     int minRunSize = floor(avgRunSize);
     int maxRunSize = ceil(avgRunSize); // if there's a fraction, max size is just +1 of the min
@@ -47,26 +58,26 @@ int main(){
     printf("Average Run Size: %.2f\n", avgRunSize);
     printf("Minimum Run Size: %d\n", minRunSize);
     printf("Maximum Run Size: %d\n", maxRunSize);
-    for (int l=0;l<70;l++){
-        printf("%d ",runs[l].indices);
-        printf("%d ",runs[l].length);
+    for (int l = 0; l < 70; l++) {
+        printf("%d ", runs[l].indices);
+        printf("%d ", runs[l].length);
         printf("\n");
     }
     printf("\n");
-    int g=runMaker(array,runs,sizeBig,minRunSize,maxRunSize)-1;
-    for (int l=0;l<70;l++){
-        printf("%d ",runs[l].indices);
-        printf("%d ",runs[l].length);
+    int g = runMaker(array, runs, sizeBig, minRunSize, maxRunSize) - 1;
+    for (int l = 0; l < 70; l++) {
+        printf("%d ", runs[l].indices);
+        printf("%d ", runs[l].length);
         printf("\n");
     }
     printf("\n");
-    int i=0;
-    for (int l=0;l<sizeBig;l++){
-        if(l==runs[i].indices) {
-            printf("    -->At %d\n",runs[i].indices);
+    int i = 0;
+    for (int l = 0; l < sizeBig; l++) {
+        if (l == runs[i].indices) {
+            printf("    -->At %d\n", runs[i].indices);
             i++;
         }
-        printf("%d ",array[l]);
+        printf("%d ", array[l]);
     }
     printf("\n");
     printf("%d", g);
@@ -79,75 +90,51 @@ int main(){
 }
 
 
-
 int runMaker(int array[], runIndices runs[], int sizeBig, int minSize, int maxSize) {
     int k = 0; // Index for indices array
     int i = 0; // Index for scanning through the main array
-    int start_descending=0;
-    bool descending=0;
-    while (i < sizeBig - 1 || descending==1 ) {
+    int number[3];
+    int realStackSize = 0;
+    int sizeofNumber = sizeof(number) / sizeof(number[0]);
+    int start_descending = 0;
+    bool descending = 0;
+    while (i < sizeBig - 1 || descending == 1) {
+
         // Record the start of the current run
-        if(i<sizeBig-1){
+        if (i < sizeBig - 1) {
             runs[k].indices = i;
-            if(k==0){
-                runs[k].length=k;
-            }
-            else{
-                runs[k].length = runs[k].indices-runs[k-1].indices;
+            if (k == 0) {
+                runs[k].length = k;
+            } else {
+                runs[k].length = runs[k].indices - runs[k - 1].indices;
             }
             k++;
+            if (k > 1) {
+                if (realStackSize < sizeofNumber) {
+                    number[realStackSize] = runs[k].indices;
+                    realStackSize++;
+                }
+            }
         }
         // Determine the direction of the run (initially ascending or descending)
-        if (array[i] < array[i+1]) {
-            ascending(&i,sizeBig,minSize,array);
+        if (array[i] < array[i + 1]) {
+            ascending(&i, sizeBig, minSize, array);
+        } else {
+            descendingFunction(&i, sizeBig, minSize, array, &start_descending, &descending);
         }
-        else {
-            descendingFunction(&i,sizeBig,minSize,array,&start_descending, &descending);
-        }
-
-
         // Move to the next potential start of a run
-        if(descending==0) i++;
+        if (descending == 0) i++;
         descending = false; // Reset the flag
     }
     // Handle the last element as the start of a run if it hasn't been included yet
-    if (i == sizeBig - 1 && k > 0 && runs[k-1].indices != i) {
+    if (i == sizeBig - 1 && k > 0 && runs[k - 1].indices != i) {
         runs[k++].indices = i;
     }
     return k;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int* binaryInsert(int firstArray[], int firstSize, int element) {
+int *binaryInsert(int firstArray[], int firstSize, int element) {
     int left = 0;
     int right = firstSize - 1;
     int insertPos;
@@ -169,18 +156,12 @@ int* binaryInsert(int firstArray[], int firstSize, int element) {
         firstArray[k] = firstArray[k - 1];
     }
     // Insert the new element
-    if(left==firstSize) firstArray[insertPos-1]=element;
+    if (left == firstSize) firstArray[insertPos - 1] = element;
     else firstArray[insertPos] = element;
     return firstArray;
 }
-void combineMerge(int firstArray[],int bigArray[],int firstArraySize, int secondArraySize){
-    int i=0;//for first array
-    int j=0;// for the bigger array
-    int k=0;//for the new sorted array
-    int sortedArray[firstArraySize+secondArraySize];
-}
-void ascending(int *i, int sizeBig, int minSize, int array[])
-{
+
+void ascending(int *i, int sizeBig, int minSize, int array[]) {
     int firstArrayStart = *i;
     int *arrayPointer = NULL;
     int count = 1;
@@ -216,18 +197,19 @@ void ascending(int *i, int sizeBig, int minSize, int array[])
         printf("\n");
     }
 }
-void descendingFunction(int *i, int sizeBig, int minSize, int array[],int *start_descending, bool *descending){
-    *start_descending=*i;
-    int firstArrayStart=*i;
-    int *arrayPointer=NULL;
-    int count=1;
+
+void descendingFunction(int *i, int sizeBig, int minSize, int array[], int *start_descending, bool *descending) {
+    *start_descending = *i;
+    int firstArrayStart = *i;
+    int *arrayPointer = NULL;
+    int count = 1;
     // Descending run or single element run
-    while (*i + 1 < sizeBig && array[*i] >= array[*i+1]) {
+    while (*i + 1 < sizeBig && array[*i] >= array[*i + 1]) {
         (*i)++;
         count++;
     }
-    int firstArrayEnd=*i;
-    *descending=1;
+    int firstArrayEnd = *i;
+    *descending = 1;
     (*i)++;
     // After identifying the end of a descending run
     if (*descending) {
@@ -242,31 +224,152 @@ void descendingFunction(int *i, int sizeBig, int minSize, int array[],int *start
             l--;
         }
     }
-    if(count<minSize && *i+1<sizeBig){
-        int firstArraySize=(firstArrayEnd-firstArrayStart)+1;
+    if (count < minSize && *i + 1 < sizeBig) {
+        int firstArraySize = (firstArrayEnd - firstArrayStart) + 1;
 
-        printf("Insertion sort started from %d to %d\n",firstArrayStart,firstArrayEnd);
-        int remaining=minSize-count;
-        int firstArray[firstArraySize+remaining]={};
-        int q=0;
-        for (int p=firstArrayStart;p<=firstArrayEnd;p++){
-            firstArray[q]=array[p];
+        printf("Insertion sort started from %d to %d\n", firstArrayStart, firstArrayEnd);
+        int remaining = minSize - count;
+        int firstArray[firstArraySize + remaining] = {};
+        int q = 0;
+        for (int p = firstArrayStart; p <= firstArrayEnd; p++) {
+            firstArray[q] = array[p];
             q++;
         }
-        for (int j=0;j<remaining;j++){
-            arrayPointer= binaryInsert(firstArray,firstArraySize+remaining,array[firstArrayEnd+1]);
+        for (int j = 0; j < remaining; j++) {
+            arrayPointer = binaryInsert(firstArray, firstArraySize + remaining, array[firstArrayEnd + 1]);
             (*i)++;
         }
-        int e=0;
-        for (int temp=firstArrayStart;temp<=firstArrayEnd+remaining;temp++){
-            array[temp]=arrayPointer[e];
+        int e = 0;
+        for (int temp = firstArrayStart; temp <= firstArrayEnd + remaining; temp++) {
+            array[temp] = arrayPointer[e];
             e++;
-            printf("%d\n",array[temp]);
+            printf("%d\n", array[temp]);
         }
-        for (int l=0;l<sizeBig;l++){
-            printf("%d ",array[l]);
+        for (int l = 0; l < sizeBig; l++) {
+            printf("%d ", array[l]);
         }
         printf("\n");
 
     }
 }
+
+void stack(int array[], int *arraySize, int stackArray[], int *realSizeStackArray, int *fullSizeStackArray, runIndices
+run[], int
+           k) {
+    for (int n = 0; n < *fullSizeStackArray; n++) {
+        if (stackArray[n] != 0) {
+            (*realSizeStackArray)++;
+        }
+    }
+    if (*realSizeStackArray < *fullSizeStackArray) {
+        stackArray[*realSizeStackArray] = run[k].indices;
+    } else if (*realSizeStackArray == 2) {
+        int firstArrayLength = run[k - 2].length;
+        int secondArrayLength = run[k - 1].length;
+        int thirdArrayLength = run[k].length;
+        //do the normal comparison
+        int xArray[firstArrayLength];
+        int yArray[secondArrayLength];
+        int zArray[thirdArrayLength];
+        if (secondArrayLength <= thirdArrayLength) {
+            //do the thing
+        }
+        if (firstArrayLength <= (secondArrayLength + thirdArrayLength)) {
+
+        }
+
+
+        //then after it is done,push the next array
+    }
+}
+
+void secondInvariantLogic(int array[], int *arraySize, int stackArray[], int *realSizeStackArray, int *fullSizeStackArray, runIndices
+run[], int k, int *firstArrayLength, int *secondArrayLength, int *thirdArrayLength) {
+    if ((*firstArrayLength) + (*secondArrayLength) < (*secondArrayLength) + (*thirdArrayLength)) {
+        //merge array X and Y into the first spot and push Z into the second spot leaving number 3 spot free
+    } else if ((*secondArrayLength) + (*thirdArrayLength) < (*firstArrayLength) + (*secondArrayLength)) {
+        //merge Y and Z and push merge them into the second spot and leave number 3 free
+    }
+
+}
+
+int combineMerge(int array[], int firstArray[], int secondArray[], int *firstArraySize, int
+*secondArraySize, int o) {
+    int i = 0;//for first array
+    int j = 0;// for the second array
+    int count = 0;
+    int winningStreak = 0; // Counter for consecutive elements from one run
+    int GALLOP_MODE_ENTER = 7; // Threshold to enter gallop mode
+    while (i < *firstArraySize && j < *secondArraySize) {
+        if (firstArray[i] < secondArray[j]) {
+            array[o] = firstArray[i];
+            o++;
+            i++;
+            count++;
+            winningStreak++;
+            if (winningStreak >= GALLOP_MODE_ENTER) {
+                int elementsToSkip = gallop(i, *firstArraySize, firstArray, secondArray[j]);
+                for (int skipped = 0; skipped < elementsToSkip; skipped++) {
+                    array[o++] = firstArray[i++];
+                }
+                count += elementsToSkip;
+                winningStreak = 0;
+            }
+        } else if (secondArray[j] < firstArray[i]) {
+            array[o] = secondArray[j];
+            o++;
+            j++;
+            count++;
+            winningStreak++;
+            if (winningStreak >= GALLOP_MODE_ENTER) {
+                int elementsToSkip = gallop(j, *secondArraySize, secondArray, firstArray[i]);
+                for (int skipped = 0; skipped < elementsToSkip; skipped++) {
+                    array[o++] = secondArray[j++];
+                }
+                count += elementsToSkip;
+                winningStreak = 0;
+            }
+        }
+    }
+    while (j < *secondArraySize) {
+        array[o] = secondArray[j];
+        o++;
+        j++;
+        count++;
+    }
+    while (i < *firstArraySize) {
+        array[o] = firstArray[i];
+        o++;
+        i++;
+        count++;
+    }
+    return count;
+}
+
+int gallop(int start, int end, int array[], int target) {
+    // Assuming 'mergePosition' is a pointer to where in the main array we should start merging
+
+    // Use binary search to find the first element in the array not less than the target
+    int insertPoint = binarySearch(array, start, end, target);
+
+    // 'insertPoint' is the position where the target should be inserted to maintain order,
+    // or, in the context of galloping, it's the position where normal merging should resume.
+
+    // Update the merge position based on the result of the binary search.
+    int elements = insertPoint - start;
+    return elements;
+}
+
+int binarySearch(int array[], int low, int high, int target) {
+    while (low < high) {
+        int mid = low + (high - low) / 2; // To avoid overflow
+        if (array[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    return low; // This will be the insertion point
+}
+
+
